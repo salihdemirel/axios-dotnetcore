46b839c36a83242d545f3db2fd5ee720190ff9f8
salih.demirel <salih.demirel@outlook.com.tr>
2019-12-01 23:40:59 +0300

    first commit

---
+++ AxiosNetCore/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AxiosNetCore.Models;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace AxiosNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public class MessageBody
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string Message { get; set; }
+        }
+
+        [HttpGet("AsJson")]
+        public JsonResult GetMessages()
+        {
+            var GetMessage = new MessageBody
+            {
+                Name = "John",
+                Email = "Doe",
+                Message = "Lorem Ipsum Dolor Sit amet..."
+            };
+
+            return Json(new MessageBody { Name = "John", Email = "Doe" , Message = "Lorem ipsum dolar sit amet" });
+        }
+
+    }
+}
---
+++ AxiosNetCore/wwwroot/js/simple_get.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () { 
+
+    $('#movieForm').on('submit', (e) => {
+
+        var searchText = $('#filmName').val();
+        getMovies(searchText);
+        e.preventDefault();
+
+    });
+});
+
+function getMovies(searchText) {
+
+    axios.get('https://www.omdbapi.com/?s=' + searchText + '&apikey=7c854682').then((response) => {
+
+        console.log(response);
+        var movies = response.data.Search;
+        var output = '';
+        $.each(movies, (index, movie) => {
+
+            output += ` 
+                <div class = "col-md-4">
+                    <div class = "well text-center">
+                        <img src = "${movie.Poster}" style="width:100%; height:300px">
+                        <h3 style="height:50px">${movie.Title}</h3>
+                    </div>
+                </div>
+            `;
+
+        });
+
+        $('#movieList').html(output);
+    })
+
+        .catch((err) => {
+
+            console.log(err);
+
+
+        });
+}
